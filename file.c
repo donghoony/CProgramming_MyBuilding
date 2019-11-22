2a54633f26f12d4e9a500f2d412e70475162694d
donghoony <aru0504@naver.com>
2019-11-22 14:18:50 +0900

    v0.46_ File directory modified

--- file.c
+++ file.c
@@ -35,7 +35,7 @@ Land* file_get_land_info(){
     Land* land_array = (Land*) malloc(sizeof(Land) * 21);
 
     // LAND_INFO
-    FILE* f = fopen("C:\\Users\\DongHoony\\CLionProjects\\KONKUK\\CProgramming_TeamProject_MyBuilding\\LAND_INFO", "r");
+    FILE* f = fopen("..\\CProgramming_Teamproject_Mybuilding\\LAND_INFO", "r");
 
     if (is_nullptr(f)) return NULL;
     get_chars_until_specific_char(f, START_CHAR);
@@ -70,9 +70,19 @@ Land* file_get_land_info(){
 }
 
 int file_get_land_pos(Land **land_arr_addr){
+    int i, num;
     Land* arr = *land_arr_addr;
-    FILE* f = fopen("C:\\Users\\DongHoony\\CLionProjects\\KONKUK\\CProgramming_TeamProject_MyBuilding\\LAND_POS_INFO", "r");
+    FILE* f = fopen("..\\CProgramming_Teamproject_Mybuilding\\LAND_POS_INFO", "r");
 
+    if (is_nullptr(f)) return NULL;
+    get_chars_until_specific_char(f, START_CHAR);
+
+    for(i = 0; i < 21; i++){
+        fscanf(f, "%d", &num);
+        if (num != i) printf("WRONG MATCH!\n");
+
+
+    }
 
     fclose(f);
 }
--- land.h
+++ land.h
@@ -13,6 +13,7 @@ typedef struct land{
     int land_multiply; //배수 설정 (페스티벌)
     int label; //누구건지 0, 1, 2 (소유 x, 플레이어, 봇)
     int level; // 0000 -> 2진수를 통한 별장, 빌딩, 호텔, 랜드마크 여부
+
 }Land;
 
 
