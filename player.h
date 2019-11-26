c4a0b7d427d3af56c87ad5fc47676cd12f52ab24
seonwook0215 <seonwook0215@naver.com>
2019-11-27 01:26:08 +0900

    아 몰라

--- player.h
+++ player.h
@@ -1,13 +1,16 @@
-//
-// Created by DongHoony on 2019-11-20.
-//
+#include<stdio.h>
 
-#pragma  once
-typedef struct player{
-    int money;
-    int position;
-    int label; //->identify bot or not
-    int lap; // 몇 바퀴 돌았는지
+typedef struct player {
+	int money;
+	int position;
+	char* name;
+	int label; //->identify bot or not
+	int lap; // 몇 바퀴 돌았는지
 }Player;
+
+//random.h
+int rand_roll_dice(Player* user);
+void* make_user_name(Player* user);
+int rand_house_person();
 
 void player_move(Player* p, int move_value);
