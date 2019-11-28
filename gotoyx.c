205eb2eeb129f2d5e36acb1efc3179564ac264b7
donghoony <aru0504@naver.com>
2019-11-29 01:44:42 +0900

    Added gotoyx_print func

--- gotoyx.c
+++ gotoyx.c
@@ -4,9 +4,13 @@
 
 #include <wincon.h>
 #include <winbase.h>
+#include <stdio.h>
 
 void gotoyx(int y, int x){
     COORD pos = {x, y};
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
-void gotoyx_print(int y, int x, char* arg);
+void gotoyx_print(int y, int x, char* arg){
+    gotoyx(y, x);
+    printf("%s", arg);
+}
