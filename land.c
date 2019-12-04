aca43708f11e66c75c847c7a2539318385b35053
jaeuk520 <jaeuk520@naver.com>
2019-12-04 16:33:51 +0900

    testing

--- land.c
+++ land.c
@@ -11,7 +11,7 @@
 // suppose player has enough money  (functions below)
 
 int land_buy_build(Player* p, Land* land, Resident* r, int level){
-    const int PEOPLE[] = {0, 2, 3, 4};
+    const int PEOPLE[] = {0, 0, 2, 3, 4};
     int pos = land->land_position;
     land->label = p->label;
     if (level == ONLY_LAND) return OK;
@@ -49,9 +49,9 @@ int land_check_valid(){
 
 // level은 ONLY_LAND, VILLA, BUILDING, HOTEL, LANDMARK로 전달할 것!
 int land_buy(Player* p, Land* land, Resident* res, int level){
-    // [12345 land villa building hotel landmark]
+    // [12345 land villa building hotel landmark
     // User buys land with level
-    const double MULTIPLY[] = {1.0, 1.2, 1.5, 1.7, 2.0};
+    const double MULTIPLY[] = {0, 1.0, 1.2, 1.5, 1.7, 2.0};
     int price = land->land_price * MULTIPLY[level];
     // if you have enough money
     if (money_compare(p->money, price)){
