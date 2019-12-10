a88151b80fa8b4573cc6f833b3c69b4f80e7e0d9
seonwook0215 <seonwook0215@naver.com>
2019-12-10 18:05:21 +0900

    인트로 늦어서 ㅈㅅ 2번째꺼도 오늘안에 올릴게요

---
+++ intro_1.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdio.h>
+#define BLA "\x1b[30m"
+#define RED "\x1b[91m"
+#define YEL "\x1b[33m"
+#define WHI "\x1b[37m"
+#define RES "\x1b[0m"
+#define BYE "\x1b[93m"
+#define netmarble "..\\netmarble.wav"
+void screen_1();
+void screen_2();
+void screen_3();
+void screen_0();
+void play_intro();
