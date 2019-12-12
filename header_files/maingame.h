7ac05d9dcfa0d005d07798c518e47870120c9e5f
이현희 <starcraft0529@gmail.com>
2019-12-13 00:52:44 +0900

    bug fix

--- header_files/maingame.h
+++ header_files/maingame.h
@@ -2,13 +2,13 @@
 
 #define GAME_NUMBER 1
 
-int SetBet(int* bet);
+//int SetBet(int* bet);
 int ChooseGame();
 int GoOrStop(int* go_or_stop);
 
-int PlayHeadOrTail(int bet, int* multiple, int* win_or_lose);
+int PlayHeadOrTail(int* multiple, int* win_or_lose);
 int HeadOrTail0(int* guess, int* menu, GameObject* SystemMenu, GameObject* SystemCursor, GameObject* Player);
 int HeadOrTail1(int* menu, GameObject* SystemMenu, GameObject* Coin);
 int HeadOrTail2(int* menu, int* guess, int* win_or_lose, int* multiple, GameObject* SystemMenu, GameObject* Coin, GameObject* Player);
-int PlayShellGame(int bet, int* multiple, int* win_or_lose);
-int SetResult(int*multiple, int* bet, int* win_or_lose);
+int PlayShellGame(int* multiple, int* win_or_lose);
+int SetResult(int*multiple, int* win_or_lose);
--- source_files/maingame.c
+++ source_files/maingame.c
@@ -2,9 +2,9 @@
 
 int PlayMaingame() {
 
-	int (*PlayGame[GAME_NUMBER])(int bet, int* multiple, int* win_or_lose);
+	int (*PlayGame[GAME_NUMBER])(int* multiple, int* win_or_lose);
 	int choose_game;
-	int bet;
+	//int bet;
 	int multiple = 0;
 	int win_or_lose = 0;
 	int go_or_stop = 1;
@@ -13,93 +13,97 @@ int PlayMaingame() {
 	gotoyx_clear();
 	PlayGame[0] = PlayHeadOrTail;
 
-	SetBet(&bet);
+	//SetBet(&bet);
 
 	
 	while (multiple < 3 && win_or_lose == 0 && go_or_stop == 1) {
 
 		choose_game = ChooseGame();
 
-		PlayGame[choose_game](bet, &multiple, &win_or_lose);
+		PlayGame[choose_game](&multiple, &win_or_lose);
 
 		if (win_or_lose == 0 && multiple <2)
 			GoOrStop(&go_or_stop);
 
 	}
 
-	result = SetResult(&multiple, &bet, &win_or_lose);
-
 	gotoyx_clear();
+
+	result = SetResult(&multiple, &win_or_lose);
 
 	return result;
 }
-int SetBet(int* bet) {
 
-	int arrow = 0;
+//
+//int SetBet(int* bet) {
+//
+//	int arrow = 0;
+//
+//	//기능이 하나뿐이면 굳이 게임오브젝트로 선언하지 말고 COMPONENT자체가 오브젝트인 것 처럼 사용도 가능
+//	RenderComponent* Menu = NewRenderComponent(RENDER_COMPONENT);
+//
+//	//기능이 두 개 이상이면 게임오브젝트로 선언해야함
+//	GameObject* Cursor = NewGameObject(SYSTEMCURSOR_GAMEOBJECT);
+//
+//	//물론 기능이 하나여도 게임오브젝트로 선언해도 됨
+//	GameObject* Player = NewGameObject(PLAYER_GAMEOBJECT);
+//
+//	//게임오브젝트인 Cursor에 Render 컴포넌트와 InputComponent 컴포넌트, Trigger 컴포넌트 추가
+//	RenderComponent* CursorRender = NewRenderComponent(RENDER_COMPONENT);
+//	Cursor->InsertComponent(Cursor->cthis, RENDER_COMPONENT, (ComponentBase*)CursorRender);
+//	CursorComponent* CursorCursor = NewCursorComponent(CURSOR_COMPONENT);
+//	Cursor->InsertComponent(Cursor->cthis, CURSOR_COMPONENT, (ComponentBase*)CursorCursor);
+//
+//	InputComponent* PlayerInput = NewInputComponent(INPUT_COMPONENT);
+//	Player->InsertComponent(Player->cthis, INPUT_COMPONENT, (ComponentBase*)PlayerInput);
+//	
+//	/* Render 컴포넌트 데이터 불러오기 */
+//	Menu->SetInputHeight(Menu->cthis, 7);
+//	Menu->SetInputWidth(Menu->cthis, 100);
+//	Menu->GetData(Menu->cthis, "..\\data\\SetBetMenu.txt");
+//	((RenderComponent*)Cursor->GetComponent(Cursor->cthis, RENDER_COMPONENT))->SetInputHeight(((RenderComponent*)Cursor->GetComponent(Cursor->cthis, RENDER_COMPONENT))->cthis, 1);
+//	// CursorRender->SetInputHeight(CursorRender->cthis, 1);과 같다
+//	((RenderComponent*)Cursor->GetComponent(Cursor->cthis, RENDER_COMPONENT))->SetInputWidth(((RenderComponent*)Cursor->GetComponent(Cursor->cthis, RENDER_COMPONENT))->cthis, 3);
+//	((RenderComponent*)Cursor->GetComponent(Cursor->cthis, RENDER_COMPONENT))->GetData(((RenderComponent*)Cursor->GetComponent(Cursor->cthis, RENDER_COMPONENT))->cthis, "..\\data\\SetBetCursor.txt");
+//	// CursorRender->GetData(CursorRender->cthis, "SetBetCursor.txt");와 같다
+//
+//	// Cursor 컴포넌트 설정
+//	((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->max = 2;
+//	((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->min = 0;
+//
+//	while (1) {
+//
+//		//Player게임 오브젝트에서 InputComponent 이용해서 입력값 받아오기
+//		((InputComponent*)Player->GetComponent(Player->cthis, INPUT_COMPONENT))->input = ((InputComponent*)Player->GetComponent(Player->cthis, INPUT_COMPONENT))->GetInput(((InputComponent*)Player->GetComponent(Player->cthis, INPUT_COMPONENT))->cthis);
+//		
+//		//입력값이 오른쪽 화살표이면
+//		if (((InputComponent*)Player->GetComponent(Player->cthis, INPUT_COMPONENT))->input == RIGHT_ARROW && ((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->arrow < ((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->max) {
+//			((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->arrow++;
+//			gotoyx_clear();
+//		}
+//		//입력값이 왼쪽 화살표이면
+//		else if (((InputComponent*)Player->GetComponent(Player->cthis, INPUT_COMPONENT))->input == LEFT_ARROW && ((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->arrow > ((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->min) {
+//			((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->arrow--;
+//			gotoyx_clear();
+//		}
+//		//입력값이 엔터이면
+//		else if (((InputComponent*)Player->GetComponent(Player->cthis, INPUT_COMPONENT))->input == ENTER) {
+//			*bet = ( ((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->arrow + 1 ) * 1000;
+//			gotoyx_clear();
+//			break;
+//		}
+//
+//		Menu->Render(Menu->cthis, 0, 0, 0,  15);
+//		((RenderComponent*)Cursor->GetComponent(Cursor->cthis, RENDER_COMPONENT))->Render(((RenderComponent*)Cursor->GetComponent(Cursor->cthis, RENDER_COMPONENT))->cthis, ((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->arrow *3, 20, 0, 8);
+//		((InputComponent*)Player->GetComponent(Player->cthis, INPUT_COMPONENT))->input = 0;
+//	}
+//
+//	gotoyx_clear();
+//
+//	return 0;
+//}
 
-	//기능이 하나뿐이면 굳이 게임오브젝트로 선언하지 말고 COMPONENT자체가 오브젝트인 것 처럼 사용도 가능
-	RenderComponent* Menu = NewRenderComponent(RENDER_COMPONENT);
 
-	//기능이 두 개 이상이면 게임오브젝트로 선언해야함
-	GameObject* Cursor = NewGameObject(SYSTEMCURSOR_GAMEOBJECT);
-
-	//물론 기능이 하나여도 게임오브젝트로 선언해도 됨
-	GameObject* Player = NewGameObject(PLAYER_GAMEOBJECT);
-
-	//게임오브젝트인 Cursor에 Render 컴포넌트와 InputComponent 컴포넌트, Trigger 컴포넌트 추가
-	RenderComponent* CursorRender = NewRenderComponent(RENDER_COMPONENT);
-	Cursor->InsertComponent(Cursor->cthis, RENDER_COMPONENT, (ComponentBase*)CursorRender);
-	CursorComponent* CursorCursor = NewCursorComponent(CURSOR_COMPONENT);
-	Cursor->InsertComponent(Cursor->cthis, CURSOR_COMPONENT, (ComponentBase*)CursorCursor);
-
-	InputComponent* PlayerInput = NewInputComponent(INPUT_COMPONENT);
-	Player->InsertComponent(Player->cthis, INPUT_COMPONENT, (ComponentBase*)PlayerInput);
-	
-	/* Render 컴포넌트 데이터 불러오기 */
-	Menu->SetInputHeight(Menu->cthis, 7);
-	Menu->SetInputWidth(Menu->cthis, 100);
-	Menu->GetData(Menu->cthis, "..\\data\\SetBetMenu.txt");
-	((RenderComponent*)Cursor->GetComponent(Cursor->cthis, RENDER_COMPONENT))->SetInputHeight(((RenderComponent*)Cursor->GetComponent(Cursor->cthis, RENDER_COMPONENT))->cthis, 1);
-	// CursorRender->SetInputHeight(CursorRender->cthis, 1);과 같다
-	((RenderComponent*)Cursor->GetComponent(Cursor->cthis, RENDER_COMPONENT))->SetInputWidth(((RenderComponent*)Cursor->GetComponent(Cursor->cthis, RENDER_COMPONENT))->cthis, 3);
-	((RenderComponent*)Cursor->GetComponent(Cursor->cthis, RENDER_COMPONENT))->GetData(((RenderComponent*)Cursor->GetComponent(Cursor->cthis, RENDER_COMPONENT))->cthis, "..\\data\\SetBetCursor.txt");
-	// CursorRender->GetData(CursorRender->cthis, "SetBetCursor.txt");와 같다
-
-	// Cursor 컴포넌트 설정
-	((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->max = 2;
-	((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->min = 0;
-
-	while (1) {
-
-		//Player게임 오브젝트에서 InputComponent 이용해서 입력값 받아오기
-		((InputComponent*)Player->GetComponent(Player->cthis, INPUT_COMPONENT))->input = ((InputComponent*)Player->GetComponent(Player->cthis, INPUT_COMPONENT))->GetInput(((InputComponent*)Player->GetComponent(Player->cthis, INPUT_COMPONENT))->cthis);
-		
-		//입력값이 오른쪽 화살표이면
-		if (((InputComponent*)Player->GetComponent(Player->cthis, INPUT_COMPONENT))->input == RIGHT_ARROW && ((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->arrow < ((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->max) {
-			((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->arrow++;
-			gotoyx_clear();
-		}
-		//입력값이 왼쪽 화살표이면
-		else if (((InputComponent*)Player->GetComponent(Player->cthis, INPUT_COMPONENT))->input == LEFT_ARROW && ((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->arrow > ((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->min) {
-			((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->arrow--;
-			gotoyx_clear();
-		}
-		//입력값이 엔터이면
-		else if (((InputComponent*)Player->GetComponent(Player->cthis, INPUT_COMPONENT))->input == ENTER) {
-			*bet = ( ((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->arrow + 1 ) * 1000;
-			gotoyx_clear();
-			break;
-		}
-
-		Menu->Render(Menu->cthis, 0, 0, 0,  15);
-		((RenderComponent*)Cursor->GetComponent(Cursor->cthis, RENDER_COMPONENT))->Render(((RenderComponent*)Cursor->GetComponent(Cursor->cthis, RENDER_COMPONENT))->cthis, ((CursorComponent*)Cursor->GetComponent(Cursor->cthis, CURSOR_COMPONENT))->arrow *3, 20, 0, 8);
-		((InputComponent*)Player->GetComponent(Player->cthis, INPUT_COMPONENT))->input = 0;
-	}
-
-	gotoyx_clear();
-
-	return 0;
-}
 int ChooseGame() {
 
 	srand((unsigned)time(NULL));
@@ -153,17 +157,18 @@ int GoOrStop(int* go_or_stop) {
 
 	}
 }
-int SetResult(int* multiple, int* bet, int* win_or_lose) {
+
+int SetResult(int* multiple, int* win_or_lose) {
 
 	if (*win_or_lose == 0) {
-		return (*bet)*(*multiple+1);
+		return *multiple;
 	}
 	else if (*win_or_lose == 1) {
-		return (*bet) * (*multiple+1) * (-1);
+		return 0;
 	}
 }
 
-int PlayHeadOrTail(int bet, int* multiple, int* win_or_lose) {
+int PlayHeadOrTail(int* multiple, int* win_or_lose) {
 
 	//게임 오브젝트 선언
 	GameObject* Player = NewGameObject(PLAYER_GAMEOBJECT);
